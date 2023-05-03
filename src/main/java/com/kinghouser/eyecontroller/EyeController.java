d91cc17beeed50c9875852dd724dc87107e4a4c2
Juvoo <lkinghouser@gmail.com>
2023-05-02 19:43:56 -0500

    Commit

---
+++ src/main/java/com/kinghouser/eyecontroller/EyeController.java
@@ -0,0 +1,11 @@
+package com.kinghouser.eyecontroller;
+
+import net.fabricmc.api.ModInitializer;
+
+public class EyeController implements ModInitializer {
+
+    @Override
+    public void onInitialize() {
+
+    }
+}
---
+++ src/main/java/com/kinghouser/eyecontroller/client/EyeControllerClient.java
@@ -0,0 +1,11 @@
+package com.kinghouser.eyecontroller.client;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class EyeControllerClient implements ClientModInitializer {
+
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
